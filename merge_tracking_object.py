5d5fbfdbf1dd1df4da873b45a40d1a0bec3e7445
ldn694 <70439334+ldn694@users.noreply.github.com>
2023-12-07 22:53:43 +0700

    Changed IMAGE_PADDING to 234

--- merge_tracking_object.py
+++ merge_tracking_object.py
@@ -8,7 +8,7 @@ import requests
 import matplotlib.pyplot as plt
 
 def add_white_rectangle(img_path, online = False):
-    IMAGE_PADDING = 218
+    IMAGE_PADDING = 234
     if (online == False):
         img = Image.open(img_path).convert("RGB")
     else:
